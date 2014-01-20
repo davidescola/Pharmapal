7f6813fcf6cb730d1b1402f53bf1b6cdc946bd69
davidescola <scola.davide@gmail.com>
2014-01-20 15:20:15 +0100

    Commit 20/01/2014 15:20

--- Farmapal/src/com/farmapal/DettaglioFarmacoActivity.java
+++ Farmapal/src/com/farmapal/DettaglioFarmacoActivity.java
@@ -69,74 +69,72 @@ public class DettaglioFarmacoActivity extends Activity {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
 						if(db.deleteFarmacoFromID(id_farmaco)) {
-							if(db.deletePrescrizioniFromIDFarmaco(id_farmaco)) {
-								Toast.makeText(getApplicationContext(), "farmaco eliminato", Toast.LENGTH_SHORT).show();
-								Intent returnIntent = new Intent();
-								setResult(RESULT_OK, returnIntent);
-								finish();
-							}
-							else
-								Toast.makeText(getApplicationContext(), "errore durante la cancellazione del farmaco", Toast.LENGTH_LONG).show();
+							db.deletePrescrizioniFromIDFarmaco(id_farmaco);
+							Toast.makeText(getApplicationContext(), "farmaco eliminato", Toast.LENGTH_SHORT).show();
+							Intent returnIntent = new Intent();
+							setResult(RESULT_OK, returnIntent);
+							finish();
 						}
 						else
 							Toast.makeText(getApplicationContext(), "errore durante la cancellazione del farmaco", Toast.LENGTH_LONG).show();
+					
 
-					}
-				});
+				}
+			});
 
 				alert.show();
 
-			}
-		});
-
-	}
-
-	private void setDatiFarmaco(int id_farmaco) {
-		c = db.getFarmacoFromID(id_farmaco);
-		if(c.moveToFirst()) {
-			textViewFarmaco.setText("Nome: " + c.getString(c.getColumnIndex("nome")));
-			String descrizione = 
-					c.getString(c.getColumnIndex("peso")) + " " + 
-							c.getString(c.getColumnIndex("tipo")) + " " +
-							c.getString(c.getColumnIndex("peso")) + " " +
-							"somministrazione " + c.getString(c.getColumnIndex("somministrazione"));
-			textViewDescrizione.setText(descrizione);
-			textViewPrincipiAttivi.setText("Principi attivi: " + c.getString(c.getColumnIndex("principi_attivi")));
-			textViewIndicazioni.setText("Indicazioni: " + c.getString(c.getColumnIndex("indicazioni")));
-			textViewControindicazioni.setText("Controindicazioni: " + c.getString(c.getColumnIndex("controindicazioni")));
-			textViewEffettiCollaterali.setText("Effetti collaterali: " + c.getString(c.getColumnIndex("effetti_indesiderati")));
 		}
+	});
 
+}
+
+private void setDatiFarmaco(int id_farmaco) {
+	c = db.getFarmacoFromID(id_farmaco);
+	if(c.moveToFirst()) {
+		textViewFarmaco.setText("Nome: " + c.getString(c.getColumnIndex("nome")));
+		String descrizione = 
+				c.getString(c.getColumnIndex("peso")) + " " + 
+						c.getString(c.getColumnIndex("tipo")) + " " +
+						c.getString(c.getColumnIndex("peso")) + " " +
+						"somministrazione " + c.getString(c.getColumnIndex("somministrazione"));
+		textViewDescrizione.setText(descrizione);
+		textViewPrincipiAttivi.setText("Principi attivi: " + c.getString(c.getColumnIndex("principi_attivi")));
+		textViewIndicazioni.setText("Indicazioni: " + c.getString(c.getColumnIndex("indicazioni")));
+		textViewControindicazioni.setText("Controindicazioni: " + c.getString(c.getColumnIndex("controindicazioni")));
+		textViewEffettiCollaterali.setText("Effetti collaterali: " + c.getString(c.getColumnIndex("effetti_indesiderati")));
 	}
 
-	private void initAttributes() {
-		textViewFarmaco = (TextView) findViewById(R.id.DettaglioFarmacoNomeFarmaco);
-		textViewDescrizione = (TextView) findViewById(R.id.DettaglioFarmacoDescrizioneFarmaco);
-		textViewPrincipiAttivi = (TextView) findViewById(R.id.DettaglioFarmacoPrincipiAttivi);
-		textViewIndicazioni = (TextView) findViewById(R.id.DettaglioFarmacoIndicazioni);
-		textViewControindicazioni = (TextView) findViewById(R.id.DettaglioFarmacoControindicazioni);
-		textViewEffettiCollaterali = (TextView) findViewById(R.id.DettaglioFarmacoEffettiCollaterali);
-		btnElimina = (Button) findViewById(R.id.DettaglioFarmacobuttonElimina);
+}
 
-		Bundle b = getIntent().getExtras();
-		id_farmaco = b.getInt("id_farmaco");
+private void initAttributes() {
+	textViewFarmaco = (TextView) findViewById(R.id.DettaglioFarmacoNomeFarmaco);
+	textViewDescrizione = (TextView) findViewById(R.id.DettaglioFarmacoDescrizioneFarmaco);
+	textViewPrincipiAttivi = (TextView) findViewById(R.id.DettaglioFarmacoPrincipiAttivi);
+	textViewIndicazioni = (TextView) findViewById(R.id.DettaglioFarmacoIndicazioni);
+	textViewControindicazioni = (TextView) findViewById(R.id.DettaglioFarmacoControindicazioni);
+	textViewEffettiCollaterali = (TextView) findViewById(R.id.DettaglioFarmacoEffettiCollaterali);
+	btnElimina = (Button) findViewById(R.id.DettaglioFarmacobuttonElimina);
 
-	}
+	Bundle b = getIntent().getExtras();
+	id_farmaco = b.getInt("id_farmaco");
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.dettaglio_farmaco, menu);
-		return true;
-	}
+}
 
-	@Override
-	public void onBackPressed() {
-		if(c != null)
-			c.close();
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.dettaglio_farmaco, menu);
+	return true;
+}
 
-		finish();
-	}
+@Override
+public void onBackPressed() {
+	if(c != null)
+		c.close();
+
+	finish();
+}
 
 
 
